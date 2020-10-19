97b9cf7fd516e0d48c1a4825c5f2c4d380a28a98
Thomas d'anglejan <thomasdanglejan@gmail.com>
2020-10-19 15:48:17 +0100

    Ajoutez des fichiers projet.

---
+++ Shoppingcart/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoppingcart
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int quantityApple = 0;
+            int quantityOrange = 0;
+            List<string> items = new List<string>();
+            items.Add(Convert.ToString(Product.apples));
+            items.Add(Convert.ToString(Product.apples));
+            items.Add(Convert.ToString(Product.oranges));
+            items.Add(Convert.ToString(Product.apples));
+
+            for(int i = 0; i < items.Count; i++)
+            {
+                if(items[i] == "oranges")
+                {
+                    quantityOrange++;
+                }
+                else
+                {
+                    quantityApple++;
+                }
+            }
+
+            double price = 0.60*quantityApple + 0.25*quantityOrange;
+            Console.WriteLine(price);
+            Console.ReadKey();
+        }
+    }
+}
